b7d82b8253b72f83e7d012eaaf1206fa03d617ea
gtherdgerthyhrtgefwthyujtyhfgds <42640657+gtherdgerthyhrtgefwthyujtyhfgds@users.noreply.github.com>
2018-08-23 12:51:06 +0200

    Update sdfghgfdssdfghgfd.js

--- sdfghgfdssdfghgfd.js
+++ sdfghgfdssdfghgfd.js
@@ -192,54 +192,6 @@ m.sendMessage(args)
       
       
       
-      const hastebin = require('hastebin-gen');
-client.on('message', message => {
-var PREFIX = '!';
-    if(message.content.startsWith(PREFIX + 'dis')) {
-            var args = message.content.split(' ').slice(1).join(' ');
-}
-      var array = [];
-      var i = 0;
-      if(args){
-client.users.filter(u => u.discriminator == args).map(u => {
-    if(i > 4){
-     return;
-    }
-    i = i + 1;
-
-   array.push(`${u.tag}`);
-});
-}
-hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
-    message.channel.send(`${l}`);
-}).catch(console.error);
-});
-client.on('message' , message => {
-var PREFIX = '!';
-if(message.content === `${PREFIX}dis`) {
-                      let array = [];
-                      var i = 0;
-client.users.filter(u => u.discriminator == message.author.discriminator).map(u => {
-    if(i > 4){
-     return;
-    }
-    i = i + 1;
-   array.push(`${u.tag}`);
-});
-hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
-    message.channel.send(`${l}`);
-}).catch(console.error);
-
-        }
-});
-      
-      
-      
-      
-      
-
-
-      
 
 client.on('message', message => {
      if (message.author.bot) return;
